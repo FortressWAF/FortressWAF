ac277fa673fe2a58640cef9418aa16c400a2752d
Muhammad Arya <arjunaajalahla100@gmail.com>
2026-05-25 17:13:57 +0700

    style: format tools.go with gofmt -s

--- tools/tools.go
+++ tools/tools.go
@@ -4,7 +4,7 @@ package tools
 
 import (
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/vektra/mockery/v2"
 	_ "golang.org/x/tools/cmd/goimports"
 	_ "golang.org/x/vuln/cmd/govulncheck"
-	_ "github.com/vektra/mockery/v2"
 )
